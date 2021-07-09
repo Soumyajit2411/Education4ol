37278e50596f5cb82b5bc73892fd406f097a7d8a
Soumyajit Roy <soumyajitroy2411@gmail.com>
2021-07-10 03:11:22 +0530

    pro

---
+++ Task-1/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/login.html");
+  });
+  
+
+app.get("/contact", (req, res) => {
+  res.send("Contact Me!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Me!");
+});
+app.get("/portfolio", (req, res) => {
+  res.send("Portfolio!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
